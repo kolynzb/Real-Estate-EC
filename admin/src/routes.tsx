a975d6446d97bae01d489acc7250afab520ab525
kolynzb <collinsbenda360@gmail.com>
2022-06-22 05:15:31 +0300

    Update imports in routes

--- admin/src/routes.tsx
+++ admin/src/routes.tsx
@@ -1,11 +1,23 @@
+import React from "react";
+
 import { Icon } from "@chakra-ui/react";
 import {
   MdBarChart,
   MdPerson,
-  MdHome,
+  MdHome,  
   MdLock,
   MdOutlineShoppingCart,
 } from "react-icons/md";
+
+// Admin Imports
+import MainDashboard from "./pages/admin/default";
+import NFTMarketplace from "./pages/admin/marketplace";
+import Profile from "./pages/admin/profile";
+import DataTables from "./pages/admin/dataTables";
+import RTL from "./pages/admin/rtl";
+
+// Auth Imports
+import SignInCentered from "./pages/auth/SignIn";
 
 const routes = [
   {
@@ -13,20 +25,51 @@ const routes = [
     layout: "/admin",
     path: "/default",
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    // component: MainDashboard,
+    component: MainDashboard,
+  },
+  {
+    name: "NFT Marketplace",
+    layout: "/admin",
+    path: "/nft-marketplace",
+    icon: (
+      <Icon
+        as={MdOutlineShoppingCart}
+        width="20px"
+        height="20px"
+        color="inherit"
+      />
+    ),
+    component: NFTMarketplace,
+    secondary: true,
+  },
+  {
+    name: "Data Tables",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
+    path: "/data-tables",
+    component: DataTables,
   },
   {
     name: "Profile",
     layout: "/admin",
     path: "/profile",
     icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
-    // component: Profile,
+    component: Profile,
   },
   {
     name: "Sign In",
     layout: "/auth",
     path: "/sign-in",
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    // component: SignInCentered,
+    component: SignInCentered,
+  },
+  {
+    name: "RTL Admin",
+    layout: "/rtl",
+    path: "/rtl-default",
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: RTL,
   },
 ];
+
+export default routes;
