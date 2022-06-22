53379e756cb23979e2f92fa607ebd6be4c38b26d
kolynzb <collinsbenda360@gmail.com>
2022-06-22 12:15:05 +0300

    Update Upload Component

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "./styles/App.css";
+import AuthLayout from "./layouts/auth";
+import AdminLayout from "./layouts/admin";
+// import RTLLayout from "./layouts/rtl";
+
 import { CircularProgress } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <Router>{/* <Route path="/" element={} /> */}</Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={`/auth`} element={<AuthLayout />} />
+        <Route path={`/`} element={<AdminLayout />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
